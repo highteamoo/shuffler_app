18e435d334c958e70730a61a764b5bf1d7b46cd0
horoshevskiy.y <horoshevskiy.y@pf5ev0a3.foreman-test.vm.prod-5.cloud.dataline>
2025-05-27 18:41:21 +0300

    change name

--- mega-tools/1.0.0/src/app.py
+++ mega-tools/1.0.0/src/app.py
@@ -55,7 +55,7 @@ os._exit = sys.exit
 class Tools(AppBase):
     __version__ = "1.2.0"
     app_name = (
-        "Mega Tools"  # this needs to match "name" in api.yaml for WALKOFF to work
+        "Mega2 Tools"  # this needs to match "name" in api.yaml for WALKOFF to work
     )
 
     def __init__(self, redis, logger, console_logger=None):
